e8d4aba612578d1e91a4b8eedd2b65e534b4ad68
Kouyxi <victorkouichi40@gmail.com>
2025-08-29 15:53:16 -0300

    setup: primevue aura

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,4 @@
 <template>
   <h1 class="text-emerald-600 font-[Inter]" >stealerTanuki</h1>
+  <Button label="Verify" />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import Aura from '@primeuix/themes/aura';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
@@ -14,6 +16,14 @@ export default defineNuxtConfig({
     overwriting: true,
     families: {
       Inter: [300, 500, 800],
+    },
+  },
+
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
     },
   },
 });
