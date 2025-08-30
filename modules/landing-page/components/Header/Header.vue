16f6e87a6ccd17bd99667e78ac9367c2e5bbafe6
4prndz <victorkouichi40@gmail.com>
2025-08-30 20:41:59 -0300

    feat: logo and HOC to use default padding

--- modules/landing-page/components/Header/Header.vue
+++ modules/landing-page/components/Header/Header.vue
@@ -1,13 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+</script>
 
 <template>
-  <header class="w-full shadow">
+  <header class="w-full shadow shadow-white">
     <nav class="px-4 lg:px-6 py-2.5">
       <div
         class="flex justify-content md:justify-between items-center mx-auto max-w-screen-xl"
       >
         <div class="flex items-center">
-          Logo
+          <NuxtLink to="/">
+            <Logo />
+          </NuxtLink>
         </div>
         <div class="hidden md:flex">
           <Button
--- modules/landing-page/screens/Home/Home.vue
+++ modules/landing-page/screens/Home/Home.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import MainContent from "~/components/MainContent/MainContent.vue";
 import Header from "../../components/Header/Header.vue";
 import Hero from "../../components/Hero/Hero.vue";
 </script>
 
 <template>
-  <Header />
-  <Hero />
+  <MainContent>
+    <template #header>
+      <Header />
+    </template>
+    <template #content>
+      <Hero />
+    </template>
+  </MainContent>
 </template>
