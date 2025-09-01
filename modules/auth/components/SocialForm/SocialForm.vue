9298b72bbf5018e2a4075ce45055c9eba71f973e
4prndz <victorkouichi40@gmail.com>
2025-09-01 20:32:04 -0300

    feat: SocialForm and Login page

---
+++ modules/auth/components/SocialForm/SocialForm.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: "wants-login-with-github"): void;
+}>();
+
+const props = defineProps<{ loading: boolean }>();
+</script>
+
+<template>
+  <div
+    class="flex flex-col gap-2 justify-center items-center rounded p-6"
+  >
+    <h2 class="font-[Inter] text-gray-300 font-regular">
+      Entre com sua conta.
+    </h2>
+    <Button
+      :loading="props.loading"
+      label="Continuar com o Github"
+      icon="pi pi-github"
+      icon-pos="right"
+      @click="() => emit('wants-login-with-github')"
+    />
+  </div>
+</template>
---
+++ modules/auth/screens/Login/Login.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Logo from "~/components/Logo/Logo.vue";
+import SocialForm from "../../components/SocialForm/SocialForm.vue";
+
+const loading = ref<boolean>(true);
+
+const handlerLoginGithub = async () => {
+  loading.value = true;
+  // TODO: CALL API
+};
+</script>
+
+<template>
+  <section
+    class="w-full h-screen flex flex-col items-center justify-center gap-5"
+  >
+    <Logo />
+    <SocialForm
+      :loading="loading"
+      @wants-login-with-github="handlerLoginGithub"
+    />
+  </section>
+</template>
