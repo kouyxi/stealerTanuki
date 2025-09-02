801c263e65f20ee020f4579e5f4de1dec4019d5c
4prndz <victorkouichi40@gmail.com>
2025-09-01 21:43:14 -0300

    feat: Widgets created

---
+++ modules/reports/components/Widget/Condensed/Condensed.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  value: number | string;
+  label: string;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-1 flex-col bg-gray-200 p-5 rounded-xl">
+    <span class="font-[Inter] text-4xl text-primary-400 font-bold">{{
+      props.value
+    }}</span>
+    <p class="font-[Inter] text-lg text-gray-500">{{ props.label }}</p>
+  </div>
+</template>
--- modules/users/screens/PublicProfile/PublicProfile.vue
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import PublicHeadline from '../../components/PublicHeadline/PublicHeadline.vue';
-
-  
+import WidgetGroup from "~/modules/reports/components/Widget/Group/Group.vue";
+import WidgetCondensed from "~/modules/reports/components/Widget/Condensed/Condensed.vue";
+import PublicHeadline from "../../components/PublicHeadline/PublicHeadline.vue";
 </script>
 
 <template>
   <PublicHeadline />
+  <WidgetGroup>
+    <WidgetCondensed :value="10" label="Reports Total"></WidgetCondensed>
+    <WidgetCondensed :value="5" label="Reports Gratuitos"></WidgetCondensed>
+    <WidgetCondensed :value="5" label="Reports Pagos"></WidgetCondensed>
+  </WidgetGroup>
 </template>
