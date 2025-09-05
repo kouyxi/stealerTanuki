43c92ba041fa49f3d4e9cc5d36c789f50f9f9cc2
4prndz <victorkouichi40@gmail.com>
2025-09-05 17:09:55 -0300

    feat: Modal to Show Fail Payment Message

---
+++ modules/payments/components/DialogPaymentError/DialogPaymentError.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const visible = defineModel<boolean>("visible");
+</script>
+
+<template>
+  <Dialog
+    modal
+    header="Deu algum problema!"
+    :style="{ width: '400px' }"
+    v-model:visible="visible"
+  >
+    <div class="m-0 flex flex-col items-center justify-center w-full">
+      <i class="pi pi-times-circle text-primary-500 text-2xl my-5"> </i>
+
+      <p class="font-[Inter] text-gray-400 text-center my-2">
+        Sentimos muito por isso! <br />
+        Por favor, tente efetuar a compra novamente.
+      </p>
+    </div>
+    <template #footer>
+      <div class="flex justify-center w-full">
+        <Button
+          label="Voltar"
+          icon="pi pi-arrow-left"
+          @click="visible = false"
+          autofocus
+        />
+      </div>
+    </template>
+  </Dialog>
+</template>
--- modules/writeup/screens/MoreDetail/MoreDetail.vue
+++ modules/writeup/screens/MoreDetail/MoreDetail.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import PublicHeadline from "../../components/PublicHeadline/PublicHeadline.vue";
 import WriteupCodeSnippet from "../../components/CodeSnippet/CodeSnippet.vue";
-import DialogPaymentSuccess from "~/modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue";
+import LazyDialogPaymentSuccess from "~/modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue";
+import LazyDialogPaymentError from "~/modules/payments/components/DialogPaymentError/DialogPaymentError.vue";
 
 const route = useRoute();
 const isPaymentSuccessfully = ref<boolean>(false);
+const isPaymentFail = ref<boolean>(false);
 
 onMounted(() => {
-  const { success_payment } = route.query;
+  const { success_payment, fail_payment } = route.query;
 
   if (success_payment) {
     isPaymentSuccessfully.value = true;
+  }
+
+  if (fail_payment) {
+    isPaymentFail.value = true;
   }
 });
 </script>
@@ -20,5 +26,6 @@ onMounted(() => {
 
   <WriteupCodeSnippet />
 
-  <DialogPaymentSuccess v-model:visible="isPaymentSuccessfully" />
+  <LazyDialogPaymentSuccess v-model:visible="isPaymentSuccessfully" />
+  <LazyDialogPaymentError v-model:visible="isPaymentFail" />
 </template>
